0b3fbe6c43fdcdc1c3f7eca03d6f9336b992c45b
nicoscha <53896425+nicoscha@users.noreply.github.com>
2019-10-22 18:38:45 +0200

    rename amplitude to magnitude; PEP8

--- tests/test_basic_wave.py
+++ tests/test_basic_wave.py
@@ -2,6 +2,7 @@ import unittest
 import basic_wave
 from math import pi
 
+
 class BasicWave(unittest.TestCase):
     def test_init_frequency_ValueError(self):
         with self.assertRaises(ValueError):
@@ -17,19 +18,19 @@ class BasicWave(unittest.TestCase):
     def test_init_phi_value(self):
         self.assertEqual(pi, basic_wave.BasicWave(440, phi=pi).phi)
 
-    def test_init_amplitude_ValueError(self):
+    def test_init_magnitude_ValueError(self):
         with self.assertRaises(ValueError):
-            a = basic_wave.BasicWave(frequency=440, amplitude='Not a int/float')
+            a = basic_wave.BasicWave(frequency=440, magnitude='Not a int/float')
 
-    def test_init_amplitude_value(self):
+    def test_init_magnitude_value(self):
         self.assertEqual(0.749, basic_wave.BasicWave(440,
-                                                     amplitude=0.749).amplitude)
+                                                     magnitude=0.749).magnitude)
 
-    def test_init_amplitude_value_range(self):
+    def test_init_magnitude_value_range(self):
         self.assertEqual(1.0, basic_wave.BasicWave(440,
-                                                   amplitude=1.5).amplitude)
-        self.assertEqual(1.0, basic_wave.BasicWave(440,
-                                                   amplitude=-5.1).amplitude)
+                                                   magnitude=1.5).magnitude)
+        self.assertEqual(-1.0, basic_wave.BasicWave(440,
+                                                    magnitude=-5.1).magnitude)
 
     def test_init_offset_ValueError(self):
         with self.assertRaises(ValueError):
