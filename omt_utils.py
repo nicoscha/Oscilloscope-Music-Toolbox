8e4c18be8eb9038295852cd1f702af7618b42a52
nicoscha <nicoscha@users.noreply.github.com>
2021-11-10 21:56:47 +0100

    generate basic wave functions

--- omt_utils.py
+++ omt_utils.py
@@ -30,3 +30,61 @@ def multiply(signal_1: List[float], signal_2: List[float]) -> List[float]:
     if len(signal_1) != len(signal_2):
         warnings.warn('Signals have different length')
     return [_1 * _2 for (_1, _2) in zip(signal_1, signal_2)]
+
+
+def gen_sin(frequency: int, sample_rate: int, duration: int) -> List:
+    """
+    :param frequency: Frequency in Hz
+    :param sample_rate: Samples pre second
+    :param duration: Duration in samples
+    :return: List of samples
+    """
+    range_samples = range(duration)
+    samples = [sin(2 * pi * frequency * (i / sample_rate)) for i in range_samples]
+    return samples
+
+
+def gen_cos(frequency: int, sample_rate: int, duration: int) -> List:
+    """
+    :param frequency: Frequency in Hz
+    :param sample_rate: Samples pre second
+    :param duration: Duration in samples
+    :return: List of samples
+    """
+    range_samples = range(duration)
+    samples = [cos(2 * pi * frequency * (i / sample_rate)) for i in range_samples]
+    return samples
+
+
+def gen_sawtooth(frequency: int, sample_rate: int, duration: int) -> List:
+    """
+    :param frequency: Frequency in Hz
+    :param sample_rate: Samples pre second
+    :param duration: Duration in samples
+    :return: List of samples
+    """
+    range_samples = range(duration)
+    len_wave = (1 / frequency) * sample_rate
+    samples = [(i % len_wave) / len_wave for i in range_samples]
+    samples = [2 * s - 1 for s in samples]  # Scale from 0..1 to 1..-1
+    return samples
+
+def gen_triangle(frequency: int, sample_rate: int, duration: int) -> List:
+    """
+    :param frequency: Frequency in Hz
+    :param sample_rate: Samples pre second
+    :param duration: Duration in samples
+    :return: List of samples
+    """
+    range_samples = range(duration)
+    len_wave = (1 / frequency) * sample_rate
+    samples = []
+    for i in range_samples:
+        j = i % len_wave
+        if j >= len_wave / 2:
+            sample = 2 * (1 - j / len_wave)
+        else:
+            sample = 2 * (j / len_wave)
+        samples.append(sample)
+    samples = [2 * s - 1 for s in samples]  # Scale from 0..1 to 1..-1
+    return samples
