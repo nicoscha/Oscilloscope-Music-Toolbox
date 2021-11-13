26def0c0a297bb245ed59f8a1ba413ff87b8b55c
nicoscha <nicoscha@users.noreply.github.com>
2021-11-13 19:20:17 +0100

    add offset and multiply all signals

--- omt_gui/gui.py
+++ omt_gui/gui.py
@@ -2,23 +2,26 @@ from datetime import date as dt_date
 from functools import partial
 from PyQt5.QtCore import QDate, Qt
 from PyQt5.QtGui import QStandardItemModel
-from PyQt5.QtWidgets import QComboBox, QCheckBox, QDateEdit, QDoubleSpinBox, QFileDialog, QHBoxLayout, QPushButton, QRadioButton, QSpinBox, QSizePolicy, QSpacerItem, QStackedWidget, QTabWidget, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QComboBox, QCheckBox, QDateEdit, QDoubleSpinBox, QFileDialog, QHBoxLayout, QLabel, QPushButton, QRadioButton, QSpinBox, QSizePolicy, QSpacerItem, QStackedWidget, QTabWidget, QVBoxLayout, QWidget
 from typing import Callable, List, Union
 from collections import namedtuple
 
 from uuid import uuid4
-from omt_utils import gen_sin, gen_cos, gen_sawtooth, gen_triangle, gen_rectangle, write, scale
+from omt_utils import gen_sin, gen_cos, gen_sawtooth, gen_triangle, gen_rectangle, offset, write, scale, multiply
 
 SAMPLE_RATE = 48000
-SAMPLES = 48000*1
-parameter = namedtuple('parameter', 'amplitude function frequency side')
+SAMPLES = 48000*5
+parameter = namedtuple('parameter', 'amplitude function frequency offset side level head_uu')
 parameters = {}
+
+
 
 
 class Selector(QHBoxLayout):
     def build(self, uu: str, side: str) -> None:
         self.uu = uu
         self.side = side
+
         self.amplitude_spin_box = QDoubleSpinBox()
         self.amplitude_spin_box.setMaximum(10)
         self.amplitude_spin_box.setSingleStep(0.1)
@@ -27,6 +30,8 @@ class Selector(QHBoxLayout):
 
         self.combo_box = QComboBox()
         self.combo_box.addItems(['sin', 'cos', 'saw', 'tri', 'rec'])
+        if side == 'x':
+            self.combo_box.setCurrentText('cos')
         self.combo_box.currentTextChanged.connect(self.update_parameters)
 
         self.frequency_spin_box = QDoubleSpinBox()
@@ -35,17 +40,27 @@ class Selector(QHBoxLayout):
         self.frequency_spin_box.setValue(400)
         self.frequency_spin_box.valueChanged.connect(self.update_parameters)
 
+        self.offset_spin_box = QDoubleSpinBox()
+        self.offset_spin_box.setRange(-1, 1)
+        self.offset_spin_box.setSingleStep(0.02)
+        self.offset_spin_box.setValue(0)
+        self.offset_spin_box.valueChanged.connect(self.update_parameters)
+
         self.update_parameters()
 
         self.addWidget(self.amplitude_spin_box)
         self.addWidget(self.combo_box)
         self.addWidget(self.frequency_spin_box)
+        self.addWidget(self.offset_spin_box)
 
     def update_parameters(self) -> None:
         amplitude = self.amplitude_spin_box.value()
         function = self.combo_box.currentText()
         frequency = self.frequency_spin_box.value()
-        p = parameter(amplitude, function, frequency, self.side)
+        offset = self.offset_spin_box.value()
+        p = parameter(amplitude=amplitude, function=function,
+                      frequency=frequency, offset=offset, side=self.side,
+                      level=0, head_uu=None)
         parameters[self.uu] = p
         print(parameters[self.uu])
 
@@ -59,15 +74,27 @@ def calc():
     y_samples = []
     for param in parameters.values():
         f = param.frequency
+        # Signal
         signal = gen_sig[param.function](f, SAMPLE_RATE, SAMPLES)
+        # Amplitude
         if param.amplitude != 1.0:
-            samples = scale(x_samples, param.amplitude)
+            samples = scale(signal, param.amplitude)
         else:
             samples = signal
+        # Offset
+        if param.offset != 0:
+            samples = offset(samples, param.offset)
+        # Multiply
         if param.side == 'x':
-            x_samples = samples
+            if x_samples:
+                x_samples = multiply(x_samples, samples)
+            else:
+                x_samples = samples
         elif param.side == 'y':
-            y_samples = samples
+            if y_samples:
+                y_samples = multiply(y_samples, samples)
+            else:
+                y_samples = samples
     write(x_samples, y_samples, SAMPLE_RATE=SAMPLE_RATE)
     print('calc done')
 
@@ -80,7 +107,11 @@ class XYLayout(QVBoxLayout):
         add_button.setText('Add signal')
         add_button.clicked.connect(self.add_selector)
 
+        label = QLabel()
+        label.setText('   Amp         Function       f in Hz            Offset')
+
         self.addWidget(add_button)
+        self.addWidget(label)
         self.add_selector()
 
     def add_selector(self) -> None:
