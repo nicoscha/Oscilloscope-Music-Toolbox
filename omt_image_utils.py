582d89b4daa075ccc7e75c7590dcfba84142de11
nicoscha <nicoscha@users.noreply.github.com>
2022-03-15 22:48:19 +0100

    only save image when file_name is provided; calculate with normed values(-1..1)

--- omt_image_utils.py
+++ omt_image_utils.py
@@ -2,13 +2,14 @@ from typing import List
 import matplotlib.image as mat_img
 
 
-def convert_audio_to_image(data: tuple[List, List], image_size: int = 640,
+def convert_audio_to_image(data: tuple[list, list], image_size: int = 640,
                            file_name: str = 'a_to_i.png') -> None:
     """
     :param data: 2 channel audio (100%=32767)
-    :param image_size: image edge length
-    :param file_name: str name and extension optional with path
-    :return: None
+    :param image_size: Image edge length
+    :param file_name: Str name and extension optional with path.
+                      Empty file_name only returns but does not save image
+    :return: Image
     """
     if len(data) != 2:
         raise ValueError(f'Stereo audio needed {len(data)} channel provided')
@@ -18,7 +19,8 @@ def convert_audio_to_image(data: tuple[List, List], image_size: int = 640,
     offset = int(image_size / 2)
     _y, _x = data
     for (x, y) in zip(_x, _y):
-        x_i = int((x / 32767) * scaling_factor) + offset
-        y_i = int((y / 32767) * scaling_factor) + offset
+        x_i = int(x * scaling_factor) + offset
+        y_i = int(y * scaling_factor) + offset
         image[x_i][y_i] = 128
-    mat_img.imsave(file_name, image, cmap='gray')
+    if file_name:
+        mat_img.imsave(file_name, image, cmap='gray')
